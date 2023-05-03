0d65671f6bd74562a2b10b358b23b4b483f03efb
bjenn01776 <bjenn1776@verizon.net>
2023-05-02 21:55:20 -0400

    Add files via upload

---
+++ myJava.js
@@ -0,0 +1,8 @@
+
+
+    
+    function purchase(){
+        alert("intem purchased");
+        document.getElementById("sharazz").innerHTML = "Elise";
+        document.getElementById("sharazz").innerTextL= "Elise";
+    } 
